686908b9103276ef0d390b525525bb289c9cc33b
Geraldo Silva <geraldojuni@hotmail.com>
2024-01-16 21:38:21 -0300

    chore: fill data source object

---
+++ src/data/source.ts
@@ -0,0 +1,83 @@
+import Jules80 from '../assets/jules-80.jpeg';
+import JulesEarth from '../assets/jules-earth.jpeg';
+import JulesIsland from '../assets/jules-island.jpeg';
+import Dracula from '../assets/dracula.jpg';
+import Frankenstein from '../assets/frankenstein.webp';
+import NineteenEightyFour from '../assets/1984.jpg';
+import Fahrenheit451 from '../assets/fahrenheit-451.jpg';
+import AnimalFarm from '../assets/animal-farm.jpeg';
+import IceAndFire from '../assets/ice-and-fire.jpg';
+import HarryPotter from '../assets/harry-potter.jpg';
+
+export const data = [
+  {
+    title: 'Around the World in 80 Days',
+    summary:
+      'Around the World in Eighty Days is an adventure novel by the French writer Jules Verne, first published in French in 1872. In the story, Phileas Fogg of London and his newly employed French valet Passepartout attempt to circumnavigate the world in 80 days on a £20,000 wager set by his friends at the Reform Club.',
+    buyUrl: 'https://www.amazon.com/Around-World-Eighty-Days-Verne/dp/1503215156',
+    imageUrl: Jules80,
+  },
+  {
+    title: 'Journey to the Center of the Earth',
+    summary:
+      'Journey to the Center of the Earth is an 1864 science fiction novel by Jules Verne. The story involves German professor Otto Lidenbrock who believes there are volcanic tubes going toward the centre of the Earth. He, his nephew Axel, and their guide Hans descend into the Icelandic volcano Snæfellsjökull, encountering many adventures, including prehistoric animals and natural hazards, before eventually coming to the surface again in southern Italy, at the Stromboli volcano.',
+    buyUrl: 'https://www.amazon.com/Journey-Center-Earth-Jules-Verne/dp/150321544X',
+    imageUrl: JulesEarth,
+  },
+  {
+    title: 'The Mysterious Island',
+    summary:
+      "The Mysterious Island is a novel by Jules Verne, published in 1874. The original edition, published by Hetzel, contains a number of illustrations by Jules Férat. The novel is a crossover sequel to Verne's famous Twenty Thousand Leagues Under the Sea and In Search of the Castaways, though its themes are vastly different from those books. An early draft of the novel, initially rejected by Verne's publisher and wholly reconceived before publication, was titled Shipwrecked Family: Marooned With Uncle Robinson, seen as indicating the influence on the novel of Robinson Crusoe and The Swiss Family Robinson.",
+    buyUrl: 'https://www.amazon.com/Mysterious-Island-Jules-Verne/dp/1503215466',
+    imageUrl: JulesIsland,
+  },
+  {
+    title: 'Dracula',
+    summary:
+      "Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It introduced the character of Count Dracula and established many conventions of subsequent vampire fantasy. The novel tells the story of Dracula's attempt to move from Transylvania to England so that he may find new blood and spread the undead curse, and of the battle between Dracula and a small group of people led by Professor Abraham Van Helsing.",
+    buyUrl: 'https://www.amazon.com/Dracula-Bram-Stoker/dp/0486411095',
+    imageUrl: Dracula,
+  },
+  {
+    title: 'Frankenstein',
+    summary:
+      'Frankenstein; or, The Modern Prometheus is an 1818 novel written by English author Mary Shelley. Frankenstein tells the story of Victor Frankenstein, a young scientist who creates a sapient creature in an unorthodox scientific experiment.',
+    buyUrl: 'https://www.amazon.com/Frankenstein-Mary-Shelley/dp/0486282112',
+    imageUrl: Frankenstein,
+  },
+  {
+    title: '1984',
+    summary:
+      "Nineteen Eighty-Four: A Novel, often published as 1984, is a dystopian novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, Nineteen Eighty-Four centres on the consequences of totalitarianism, mass surveillance, and repressive regimentation of persons and behaviours within society.",
+    buyUrl: 'https://www.amazon.com/1984-Signet-Classics-George-Orwell/dp/0451524934',
+    imageUrl: NineteenEightyFour,
+  },
+  {
+    title: 'Fahrenheit 451',
+    summary:
+      'Fahrenheit 451 is a dystopian novel by American writer Ray Bradbury, first published in 1953. Often regarded as one of his best works, the novel presents a future American society where books are outlawed and "firemen" burn any that are found.',
+    buyUrl: 'https://www.amazon.com/Fahrenheit-451-Ray-Bradbury/dp/1451673310',
+    imageUrl: Fahrenheit451,
+  },
+  {
+    title: 'Animal Farm',
+    summary:
+      'Animal Farm is an allegorical novella by George Orwell, first published in England on 17 August 1945. The book tells the story of a group of farm animals who rebel against their human farmer, hoping to create a society where the animals can be equal, free, and happy.',
+    buyUrl: 'https://www.amazon.com/Animal-Farm-George-Orwell/dp/0451526341',
+    imageUrl: AnimalFarm,
+  },
+  {
+    title: 'A Song of Ice and Fire',
+    summary:
+      'A Song of Ice and Fire is a series of epic fantasy novels by the American novelist and screenwriter George R. R. Martin. He began the first volume of the series, A Game of Thrones, in 1991, and it was published in 1996. Martin, who initially envisioned the series as a trilogy, has published five out of a planned seven volumes.',
+    buyUrl: 'https://www.amazon.com/Game-Thrones-Song-Fire-Book/dp/0553593714',
+    imageUrl: IceAndFire,
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    summary:
+      "Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling's debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.",
+    buyUrl: 'https://www.amazon.com/Harry-Potter-Sorcerers-Stone-Rowling/dp/059035342X',
+    imageUrl: HarryPotter,
+  },
+];
